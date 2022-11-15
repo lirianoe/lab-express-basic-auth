11d843d4a748c33e7ff9b943a5ba9c5ba6a159a5
Esther Liriano <estherlirianor@gmail.com>
2022-11-15 18:24:52 -0500

    iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,45 @@
 const router = require("express").Router();
+const bcryptjs = require('bcryptjs')
+const User = require('../models/User.model')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup.hbs')
+})
+
+router.post('/signup', (req, res, next) => {
+  console.log(req.body)
+  if(!req.body.username || !req.body.password){
+    res.send('Sorry! you forgot to add an username or password')
+    return
+  }
+
+  User.findOne({ username: req.body.username })
+  .then(foundUser => {
+    if(foundUser){
+      res.send('Sorry! Username already exist')
+      return
+    }
+
+      return User.create({
+        username: req.body.username,
+        password: bcryptjs.hashSync(req.body.password)
+      })
+  })
+    .then(createdUser => {
+      console.log('here is the new user', createdUser)
+      res.send(createdUser)
+    })
+
+    .catch(err => {
+      console.log('err');
+      res.send(err)
+    })
+
+})
 
 module.exports = router;
